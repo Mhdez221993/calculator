8291f372ed78ee6067677853b973876819bdb46f
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-25 19:38:15 +0100

    Add handleClick method

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-const Button = ({button}) => {
+import { CalContext } from '../Context/CalProvider';
+
+const Button = ({value}) => {
+  const {calc, setCalc} = useContext(CalContext);
+
+  const commaClick = () => {
+    setCalc({
+      ...calc,
+      num: calc.num.toString().includes('.') ? calc.num : calc.num + value
+    })
+  }
+
+  const reset = () => {
+    setCalc({sing: '', num: 0, res: 0})
+  }
+
+  const handleClick = () => {
+    const result = {
+      '.': commaClick,
+      'C': reset,
+    }
+
+    return result[value]()
+  }
+
   return (
-    <button className='button'>
-      { button }
+    <button onClick={handleClick} className='button'>
+      { value }
     </button>
   );
 };
