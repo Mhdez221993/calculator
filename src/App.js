9166bd35b86a6c8ed8110f0689a16eb55eba6c2f
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-25 18:13:02 +0100

    Create button var & import ButtonBox and Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import Button from "./components/Button";
+import ButtonBox from "./components/ButtonBox";
 import Screen from "./components/Screen";
 import Wrapper from "./components/Wrapper";
+
+const buttons = [
+  ['C', '+-', '%', '/'],
+  [7, 8, 9, 'x'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '=']
+]
 
 function App() {
   return (
     <div>
       <Wrapper>
         <Screen />
-        <h1>App</h1>
+        <ButtonBox>
+          {
+            buttons.flat().map((btn, i) => (
+              <Button
+                key={i}
+                button={btn}
+              />
+            ))
+          }
+        </ButtonBox>
       </Wrapper>
     </div>
   );
