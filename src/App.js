8aafbe817819d8256b0526a1a51945ca4f6ad73b
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-25 17:23:57 +0100

    Import Screen compoent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Screen from "./components/Screen";
 import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div>
       <Wrapper>
+        <Screen />
         <h1>App</h1>
       </Wrapper>
     </div>
